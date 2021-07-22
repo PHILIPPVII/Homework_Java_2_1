6451f2da4f48d19963bd805be9340ee35f7d4904
PHILIPPVII <philippnofear@mail.ru>
2021-07-22 16:19:22 +0800

    Написаны условия для 2 заданий ДЗ

--- src/MainV1.java
+++ src/MainV1.java
@@ -1,9 +1,9 @@
 public class MainV1 {
     public static void main(String[] args) {
-        int Amount_Tickets = 2;
-        int Cost_Ticket = 10_000;
-        int Total = Amount_Tickets * Cost_Ticket;
-        int Miles = Total/20;
-        System.out.println(Miles);
+        int amountTickets = 2;
+        int costTicket = 10_000;
+        int total = amountTickets * costTicket;
+        int miles = total/20;
+        System.out.println(miles);
             }
 }
--- src/MainV2.java
+++ src/MainV2.java
@@ -1,12 +1,12 @@
 public class MainV2 {
     public static void main(String[] args) {
-        int Add_Money = 1100;
-        int Balance = 100 + Add_Money;
-        int Bonus = Balance / 1000;
-        if (Balance > 1000)
-            System.out.println("Поздравляем вам начислены бонусы в размере:" + Bonus + "балл");
+        int addMoney = 1100;
+        int balance = 100 + addMoney;
+        int bonus = balance / 1000;
+        if (balance > 1000)
+            System.out.println("Поздравляем вам начислены бонусы в размере:" + bonus + "балл");
         else {
-            System.out.println("Пополнен баланс на сумму:" + Add_Money + "рублей");
+            System.out.println("Пополнен баланс на сумму:" + addMoney + "рублей");
 
         }
     }
