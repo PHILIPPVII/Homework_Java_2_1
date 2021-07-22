18679d745aac1ee8dcf14d322dee32da2a3b53cd
PHILIPPVII <philippnofear@mail.ru>
2021-07-22 13:40:13 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int Amount_Tickets = 2;
+        int Cost_Ticket = 10_000;
+        int Total = Amount_Tickets * Cost_Ticket;
+        int Miles = Total/20;
+        System.out.println(Miles);
+            }
+}
